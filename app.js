6106670679ed76b4e9a5bc713439069f235883ef
Lewi Gilamichael <dr.lewi@gmail.com>
2017-07-02 16:19:46 -0400

    more plans

--- app.js
+++ app.js
@@ -4,14 +4,16 @@
 // State object
 const ticTacToeObj = {
   Boxes: [[],[],[]]
-
-}
-
+//click Value:
+// 
+};
 
 // Functions that modify state
-
+//put X/O vlaue in Boxes
+//evaluate win and tie conditions
 
 // Render functions
+//Render updated boxes
 
 
 // event listener functions
