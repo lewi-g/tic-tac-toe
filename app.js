c9422d0ddde2dc0d0302837c0d8cb64220916692
Morgan Kestner <morganleekestner@gmail.com>
2017-07-02 18:11:29 -0400

    test onclick  event listener

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+$(document).ready(
+  clickOnBox()
+);
 
 // State object
 const ticTacToeObj = {
@@ -10,10 +13,22 @@ const ticTacToeObj = {
 
 // Functions that modify state
 //put X/O vlaue in Boxes
+const x = $('.x').attr.src;
+const o = $('.o').attr.src;
+
+const putXInBoxes = () => {
+  // populates a specific box div 
+};
+
 //evaluate win and tie conditions
 
 // Render functions
 //Render updated boxes
 
 
-// event listener functions
+// event listener functions 
+const clickOnBox = () => {
+  $('.box1').on("click", () => alert("meee!!!") )
+};
+
+
