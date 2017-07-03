e97b12990bf875d6bec150f4bbe9c9e56776bfea
Lewi Gilamichael <dr.lewi@gmail.com>
2017-07-03 02:40:44 -0400

    identified id of box clicked, finding box in stateobject

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 'use strict';
 
-$(document).ready(
-  clickOnBox()
-);
 
 // State object
 const ticTacToeObj = {
-  Boxes: [[],[],[]]
-//click Value:
+  Boxes: [
+    [{box1:'one', box2:'two' , box3: 'three'}],
+    // [{box4 , box5 , box6}],
+    // [{box7 , box8 , box9}]
+  ],
+  currentImageX: false
 // 
 };
 
@@ -15,10 +16,28 @@ const ticTacToeObj = {
 //put X/O vlaue in Boxes
 const x = $('.x').attr.src;
 const o = $('.o').attr.src;
-
-const putXInBoxes = () => {
-  // populates a specific box div 
+const row1 = ticTacToeObj.Boxes[0];
+const row1Obj = Object.keys(row1);
+const row2 = ticTacToeObj.Boxes[1];
+const row3 = ticTacToeObj.Boxes[2];
+const trackLastImage = () => {
+  if (ticTacToeObj.currentImageX) {
+    ticTacToeObj.currentImageX = !ticTacToeObj.currentImageX;
+  }
+// populates a targetted box div with inage
+// image will be picked based on last image picked 
 };
+
+const putImageInBox = (boxId) => {
+  console.log(boxId);
+  if (row1Obj.find(function(key) { row1Obj.key === boxId;})) {
+    console.log('hey there?');
+    row1.key = 'isthis the right thing';
+    console.log(row1.key);
+  }
+};
+
+
 
 //evaluate win and tie conditions
 
@@ -28,7 +47,12 @@ const putXInBoxes = () => {
 
 // event listener functions 
 const clickOnBox = () => {
-  $('.box1').on("click", () => alert("meee!!!") )
+  $('.box').on('click', () => {let boxId = event.target.id;
+    putImageInBox(boxId);
+    console.log('did we get this far');
+  });
 };
 
-
+$(document).ready(
+  clickOnBox()
+);
